cb1365d7731ba64f754784c619d2f5d8ed247958
vatsal-http <vatsal.chaturvedi0020@gmail.com>
2026-01-06 11:29:03 +0530

    Fix case-sensitive import issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 import Hero from "./components/Hero";
 import Skills from "./components/skills";
 import Projects from "./components/Projects";
